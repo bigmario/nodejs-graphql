96e2d187ae1b344e6c9f9a1ebc5364b9f8e656e8
Mario Castro <mariocastro.pva@gmail.com>
2023-01-09 14:26:24 -0400

    initial commit

---
+++ src/config/config.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  isProd: process.env.NODE_ENV === 'production',
+  isDev: process.env.NODE_ENV === 'development',
+  port: process.env.PORT || 3000,
+  dbUrl: process.env.DATABASE_URL,
+  apiKey: process.env.API_KEY,
+  jwtSecret: process.env.JWT_SECRET,
+  smtpEmail: process.env.SMTP_EMAIL,
+  smtpPassword: process.env.SMTP_PASSWORD,
+  smtpPort: process.env.SMTP_PORT,
+  smtpHost: process.env.SMTP_HOST
+
+}
+
+module.exports = { config };
